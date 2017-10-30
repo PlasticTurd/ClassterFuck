72885143fff95d6e859f5677960f00d6130499ba
PlasticTurd <jamiee.steward@gmail.com>
2017-10-30 19:46:20 +1100

    minor update #1
    -added text to show the user what to enter to select their move
    
    -added do while loop to ensure that invalid inputs are rejected, instead of defaulting to a move the player did not wish to make

--- src/javaprojectbodgev1/Player1.java
+++ src/javaprojectbodgev1/Player1.java
@@ -18,21 +18,31 @@ public class Player1 extends Player{
     
     public Roshambo generateRoshambo(){
         Scanner scanner = new Scanner( System.in );
-        System.out.println("Please select your move"); //specifications required that i implement it in this class, might as well give it a purpose
+        
+        Boolean ValidInputMoveSelect = false;
+        do {
+        System.out.println("Please select your move. for the time being, the only accepted inputs are R, P, and S, for Rock, Paper, and Scissors respectively. this will be changed but there are more fundamental issues to be fixed that are of a higher priority."); //specifications required that i implement it in this class, might as well give it a purpose
         String PlayerSelect = scanner.nextLine();
         switch (PlayerSelect){
             case "R":
                 Roshambo = Roshambo.ROCK;
+                ValidInputMoveSelect = true;
                 break;
             case "P":
                 Roshambo = Roshambo.PAPER;
+                ValidInputMoveSelect = true;
                 break;
             case "S":
                 Roshambo = Roshambo.SCISSORS;
+                ValidInputMoveSelect = true;
                 break;
             case "QUIT":
                 System.exit(0);
+            default:
+                System.out.println("invalid input, please try again");
+                ValidInputMoveSelect = false;
     }
+        } while (ValidInputMoveSelect == false);
         return Roshambo; 
 }
 }
